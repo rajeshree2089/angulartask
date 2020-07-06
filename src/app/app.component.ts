aeb66a538066852cf8f5c6ec73b3562fd816fa06
rajeshree <rajeshree.kanojiya@sphm.co.in>
2020-07-06 16:47:17 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogContentExampleDialogComponent } from './dialog-content-example-dialog/dialog-content-example-dialog.component'
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularJira';
+
+
+
+  backlogs = [
+    'Task1',
+    'Task2',
+    'Task3',
+    'Task4',
+    'Task5'
+  ];
+
+  developement = [
+    'Task6',
+    'Task7',
+    'Task8',
+    'Task9'
+  ];
+
+  codereview = [
+    'Task10',
+    'Task11',
+    'Task12',
+    'Task13'
+  ];
+
+  acceptance = [
+    'Task15',
+    'Task16'
+   
+  ];
+ 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+  constructor(public dialog: MatDialog) {}
+
+  sendValue : string = "Add Task";
+  tmp : string;
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogContentExampleDialogComponent, {
+      width: '350px',
+      disableClose:true,
+      data: { pageValue: this.sendValue }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.backlogs.push(result.data);
+     
+     
+    });
+
+  }
+
+  
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogContentExampleDialogComponent } from './dialog-content-example-dialog/dialog-content-example-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogContentExampleDialogComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatGridListModule,
+    MatDialogModule,
+    FormsModule
   ],
+  entryComponents: [
+    DialogContentExampleDialogComponent
+ ],
   providers: [],
   bootstrap: [AppComponent]
 })
